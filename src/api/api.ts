4900fbe5b670e63ef310824fef3d4fb83d0b3e6b
Sydnee Woodhouse <svwoodhouse@gmail.com>
2024-07-19 17:37:01 -0400

    Put fetchData() in api folder for global access

---
+++ src/api/api.ts
@@ -0,0 +1,5 @@
+export async function fetchData(){
+    const response = await fetch('https://dummyjson.com/products')
+    const data = await response.json()
+    return data
+}
--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Products } from "../global/types";
-import '../styles/Product.css'
+import '../styles/ProductPage.css'
 import ProductCard from "./ProductCard";
-
+import { fetchData } from "../api/api";
 const ProductPage: React.FC = () => {
     const navigate = useNavigate();
 
     const [products, setProducts] = useState<Products[]>([])
+    const [pageNumber, setPageNumber] = useState(0);
 
     useEffect(() =>{
-        async function fetchData(){
-            const response = await fetch('https://dummyjson.com/products')
-            const data = await response.json()
-            setProducts([...data.products])
+        async function getProducts(){
+            const data  = await fetchData()
+            setProducts([...data.products])  
         }
-        fetchData()        
+        getProducts()
     },[])
 
     const handleClick = (productId: number) => {
@@ -23,12 +23,19 @@ const ProductPage: React.FC = () => {
     }
 
     return (
-        <div className="products-page">
-            {
-                products.map((product)=>{
-                    return (<ProductCard key={product.id} props={product} onClick={handleClick}/>)
-                })
-            }
+        <div>
+            <div className="products-page-items">
+                {
+                    products.map((product)=>{
+                        return (<ProductCard key={product.id} props={product} onClick={handleClick}/>)
+                    })
+                }
+
+            </div>
+            <div className="products-page-footer">
+                <button>Prev</button>
+                <button>Next</button>
+            </div>
         </div>
     )
 }
